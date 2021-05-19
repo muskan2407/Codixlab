5b1c7ea6a0ce39d57052f2b85bdef260310a12de
muskan2407 <muskans2407@gmail.com>
2021-05-19 15:07:52 +0530

    Day 2 Assignments

---
+++ src/Day_2/ArithmaticOperators.java
@@ -0,0 +1,29 @@
+package Day_2;
+
+public class ArithmaticOperators {
+
+	
+		public static void main(String[] args) {
+			  // declare variables
+		    int a = 45, b = 88;
+
+		    // addition operator
+		    System.out.println("a + b = " + (a + b));
+
+		    // subtraction operator
+		    System.out.println("a - b = " + (a - b));
+
+		    // multiplication operator
+		    System.out.println("a * b = " + (a * b));
+
+		    // division operator
+		    System.out.println("a / b = " + (a / b));
+
+		    // modulo operator
+		    System.out.println("a % b = " + (a % b));
+		  }
+
+	
+	}
+
+
